6bb5f178fe33d218915010320f7a7c47c99907d6
Harsh Kumar <harsh34k@gmail.com>
2024-07-31 13:11:30 +0530

    "Changed port configuration from using 'config.port' to using 'process.env.PORT'"

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const startServer = async () => {
   // Connect database
   await connectDB();
 
-  const port = config.port || 3000;
+  const port = process.env.PORT || 3000;
 
   app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
